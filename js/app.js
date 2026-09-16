29da1f6b324d1e50d1fad17b94491176a934b690
Faisal Rehman <faisalrehman@example.com>
2026-09-16 17:13:47 +0500

    Fix double redirect: remove JS click handler, use native <a> tag navigation only + bump cache v3

--- js/app.js
+++ js/app.js
@@ -26,21 +26,9 @@
       (document.documentElement.clientWidth * 20) / 320 + "px";
   }
 
-  window.play_now_url = function () {
-    window.open("https://game.lasvegassweeps777.com/", "_blank");
-    return false;
-  };
-
   window.addEventListener("resize", setRem);
   window.addEventListener("DOMContentLoaded", function () {
     setRem();
-    var btn = document.querySelector(".downloadButton");
-    if (btn) {
-      btn.addEventListener("click", function (e) {
-        e.preventDefault();
-        play_now_url();
-      });
-    }
     var confirmBtn = document.querySelector(".popup-button");
     if (confirmBtn) {
       confirmBtn.addEventListener("click", function () {
